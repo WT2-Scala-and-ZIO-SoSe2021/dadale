783ebcda02321af617201e0c87e5bc57d1a18417
Lena <lena.vollmer@digitalunit.berlin>
2021-04-16 13:51:48 +0200

    Task 2 A & B

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,26 @@
+import scala.annotation.switch
 object Main extends App {
-  def max(arr: Array[Int]): Int = arr.reduceLeft((x, y) => x max y);
+  def max(arr: Array[Int]): Int = arr.reduceLeft((x, y) => x max y)
 
-  def min(arr: Array[Int]): Int = arr.reduceLeft((x, y) => x min y);
+  def min(arr: Array[Int]): Int = arr.reduceLeft((x, y) => x min y)
 
-  def sum(arr: Array[Int]): Int = arr.reduceLeft((x, y) => x + y);
+  def sum(arr: Array[Int]): Int = arr.reduceLeft((x, y) => x + y)
+
+  def parse(card: String): Int = {
+    card match {
+      // "A", Jack - "J", Queen - "Q", King - "K"
+      case "A" => 11
+      case "J" | "Q" | "K" => 10
+      case _ => card.toInt
+    }
+  }
+
+  def parseAll(arr: Array[String]): Array[Int] = arr.map(s => parse(s))
+
+  def values(card: Int): Array[Int] = {
+    card match {
+      case 11 => Array(1, 11)
+      case _ => Array(card)
+    }
+  }
 }
--- src/test/scala/Testing.scala
+++ src/test/scala/Testing.scala
@@ -1,21 +1,30 @@
 import org.scalatest.flatspec.AnyFlatSpec
 
 class Testing extends AnyFlatSpec {
-  "An empty Set" should "have size 0" in {
-    assert(Set.empty.size == 0)
-  }
+  val tester = Main;
 
-  "Test" should "be Test" in {
-    val test = "Test"
-    assert(test.equals("Test"))
-  }
-
-  "Main Functions" should "work" in {
-    val tester = Main;
+  "Int Array Functions" should "work" in {
     val arr = Array(2, 5, 7, 1);
 
     assert(tester.max(arr) == 7)
     assert(tester.min(arr) == 1)
     assert(tester.sum(arr) == 15)
+  }
+
+  "Parse Functions" should "work" in {
+    val cards = Array("2", "5", "7", "A", "K");
+
+    assert(tester.parse("5") == 5)
+    assert(tester.parse("A") == 11)
+    assert(tester.parse("Q") == 10)
+
+    val parsedCards = tester.parseAll(cards)
+    assert(parsedCards(0) == 2)
+    assert(parsedCards(3) == 11)
+  }
+
+  "Values Function" should "work" in {
+    assert(tester.values(4)(0) == 4 && tester.values(4).length == 1)
+    assert(tester.values(11)(0) == 1 && tester.values(11)(1) == 11 && tester.values(11).length == 2)
   }
 }
