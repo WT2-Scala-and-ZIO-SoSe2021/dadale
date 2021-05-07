3a9d231bb91a518b54cc1f42da7b942ddf05213d
Darwin Becker <darwinbecker@web.de>
2021-05-07 15:08:32 +0200

    Finished Task1 of Exercise2

--- src/main/scala/exercise2/Ior.scala
+++ src/main/scala/exercise2/Ior.scala
@@ -1,61 +1,17 @@
 package exercise2
 
-// sealed trait Ior[A] {
-//   def unit[A](left: Throwable, elem: A): Ior[A] = new Ior[A](elem)
-
-//   def flatMap[A](f: (A) => Ior[A]) = this match {
-//     case Right(b) => f(b)
-//     case Left(a)  => Left(a)
-//   }
-
-// //    def map[A](mA: Ior[A])(f: A => B): Ior[B] = flatMap(mA)(a => unit(f(a)))
-// }
-
-// case class Left[A](elem: Throwable) extends Ior[A]
-
-// case class Right[A](elem: A) extends Ior[A]
-
-// case class Both[A](left: Throwable, elem: A) extends Ior[A]
-
-// object Ior {
-//   def left[A](elem: Throwable): Left[A] = new Left(elem)
-//   def right[A](elem: A): Right[A] = new Right(elem)
-//   def both[A](left: Throwable, elem: A): Both[A] = new Both(left, elem)
-// }
-
-
-// sealed trait Ior[A] {
-//   def flatMap[A, B](a: Ior[A])(f: A => Ior[B]): Ior[B] = a match {
-//       case Right(elem) => f(elem)
-//       case Both(left, elem) => f(elem)
-//       case Left(elem) => Left(elem)
-//   }
-// }
-
-// case class Left[A](elem: Throwable) extends Ior[A]
-// case class Right[A](elem: A) extends Ior[A]
-// case class Both[A](left: Throwable, elem: A) extends Ior[A]
-
-// object Ior {
-//   def left[A](elem: Throwable): Left[A] = new Left[A](elem)
-//   def right[A](elem: A): Right[A] = new Right[A](elem)
-//   def both[A](left: Throwable, elem: A): Both[A] = new Both[A](left, elem)
-
-//   def unit[A]: A => Ior[A] = x => Both(new Throwable, x)
-
-//   def map[A, B](mA: Ior[A])(f: A => B): Ior[B] = mA.flatMap(mA)(a => unit(f(a)))
-// }
-
-// a.flatMap(x => x*2).flatMap()
-
 sealed trait Ior[A] {
-  def flatMap[A](f: A => Ior[A]): Ior[A] = this match {
-    case Right(elem)      => f(elem)
-    case Both(left, elem) => f(elem)
-    case Left(elem)       => Left(elem)
-  }
 
-  def map[A](f: A => A): Ior[A] = flatMap(a => Ior.unit(f(a)))
+  def flatMap[B](f: A => Ior[B]): Ior[B] = this match {
+    case Right(elem) => f(elem)
+    case Both(left, elem) => f(elem) match {
+      case Right(elem) => Both(left, elem)
+      case Both(left, elem) => Both(left, elem)
+      case Left(elem) => Left(elem)
+    }
+    case Left(elem) => Left(elem)
+  }
+  def map[B](f: A => B): Ior[B] = flatMap((a: A) => Ior.unit(f(a)))
 }
 
 case class Left[A](elem: Throwable) extends Ior[A]
@@ -67,5 +23,5 @@ object Ior {
   def right[A](elem: A): Right[A] = Right(elem)
   def both[A](left: Throwable, elem: A): Both[A] = Both(left, elem)
 
-  def unit[A]: A => Ior[A] = x => Both(new Throwable, x)
+  def unit[A](elem: A): Ior[A] = Right(elem)
 }
--- src/test/scala/exercise2/IorSpec.scala
+++ src/test/scala/exercise2/IorSpec.scala
@@ -1,28 +1,32 @@
-import org.scalatest.funspec.AnyFunSpec;
-import exercise2.Ior;
+package exercise2
+
+import org.scalatest.funspec.AnyFunSpec
 
 class IorSpec extends AnyFunSpec {
   describe("right") {
     it("should...") {
       val a = Ior.right(2) // Right(2)
       val b = a.map(x => x * 4) // Right(8)
+      println(b)
+
+      //val b = a.map((x: Int) => x * 4) // Right(8)
 
       val c = b.flatMap(_ => Ior.right("a string")) //Right("a string")
-
+      println(c)
       val d = c.flatMap(_ =>
         Ior.left[String](new RuntimeException("a grave error"))
       ) //Left(java.lang.RuntimeException: a grave error)
-
+      println(d)
       val e = d.map(x =>
         x + "something"
       ) //Left(java.lang.RuntimeException: a grave error)
-
+      println(e)
       val both = Ior.both(
         new RuntimeException("not fatal"),
         21
       ) //Both(java.lang.RuntimeException: not fatal,21)
       val both1 =
-        both.map((x) => x * 2) //Both(java.lang.RuntimeException: not fatal,42)
+        both.map(x => x * 2) //Both(java.lang.RuntimeException: not fatal,42)
       val both2 = both.flatMap(_ =>
         Ior.left[Int](new RuntimeException("fatal error"))
       ) //Left(java.lang.RuntimeException: fatal error)
@@ -32,6 +36,11 @@ class IorSpec extends AnyFunSpec {
       val both4 = both.flatMap(x =>
         Ior.both(new RuntimeException("another not fatal"), x * 3)
       )
+      println(both)
+      println(both1)
+      println(both2)
+      println(both3)
+      println(both4)
     }
   }
 }
