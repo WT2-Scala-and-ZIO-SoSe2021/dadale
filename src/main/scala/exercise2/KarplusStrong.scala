4d2b3cf8f630872c450e0a6b4e9c65ca33f9a2fb
David Schach <david.schach@student.htw-berlin.de>
2021-05-14 13:48:00 +0200

    added lib for task 3

---
+++ src/main/scala/exercise2/KarplusStrong.scala
@@ -0,0 +1,7 @@
+package exercise2
+
+import exercise2.lib.StdAudio
+
+class KarplusStrong {
+  def playMusic() = StdAudio.play(0.333)
+}
---
+++ src/test/scala/exercise2/KarplusStrongSpec.scala
@@ -0,0 +1,13 @@
+package exercise2
+
+package exercise2
+
+import org.scalatest.funspec.AnyFunSpec
+
+class KarplusStrongSpec extends AnyFunSpec {
+  describe("play music") {
+    it("...") {
+      new KarplusStrong().playMusic()
+    }
+  } 
+}
