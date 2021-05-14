9945512e310f68c0a0a2e2bc10c44a8878720240
David Schach <david.schach@student.htw-berlin.de>
2021-05-14 15:07:31 +0200

    task 3

--- src/main/scala/exercise2/KarplusStrong.scala
+++ src/main/scala/exercise2/KarplusStrong.scala
@@ -1,7 +1,39 @@
 package exercise2
 
 import exercise2.lib.StdAudio
+import scala.util.Random
+import scala.annotation.tailrec
 
-class KarplusStrong {
-  def playMusic() = StdAudio.play(0.333)
+object KarplusStrong {
+  def main(args: Array[String]): Unit = {
+    loop(StdAudio.play)(whiteNoise())
+  }
+
+  def whiteNoise(frequency: Int = 440, volume: Double = 1.0): Queue[Double] = {
+    if (frequency <= 0 && volume < 0.0 && volume > 1.0)
+      throw new Throwable("Incorrect parameters")
+
+    0.to(frequency).foldLeft(new Queue[Double]()) { (acc, _) =>
+      acc
+        .enqueue(Random.between(-0.5, 0.5) * volume)
+        .asInstanceOf[Queue[Double]]
+    }
+  }
+
+  def update(queue: Queue[Double]): Queue[Double] = {
+    val decay = 0.996
+    val newQueue = queue.dequeue.get
+    newQueue
+      .enqueue((newQueue.front.get + queue.front.get) / 2 * decay)
+      .asInstanceOf[Queue[Double]]
+  }
+
+  // TODO: is this really tail recursive?
+  @tailrec
+  def loop(f: Double => Unit)(queue: Queue[Double]): Unit = {
+    val updatedQueue = update(queue)
+    f(updatedQueue.front.get)
+    loop(f)(updatedQueue)
+  }
+
 }
--- src/test/scala/exercise2/KarplusStrongSpec.scala
+++ src/test/scala/exercise2/KarplusStrongSpec.scala
@@ -7,7 +7,7 @@ import org.scalatest.funspec.AnyFunSpec
 class KarplusStrongSpec extends AnyFunSpec {
   describe("play music") {
     it("...") {
-      new KarplusStrong().playMusic()
+      new KarplusStrong().whiteNoise()
     }
   } 
 }
