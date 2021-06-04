1a606e37ad3b874d8985f218d7f75558ee81716b
Lena <lena.vollmer@digitalunit.berlin>
2021-06-04 13:37:38 +0200

    Task 2 WIP

--- build.sbt
+++ build.sbt
@@ -6,3 +6,6 @@ scalaVersion := "2.13.5"
 
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.5" % "test"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.5" % "test"
+
+libraryDependencies += "dev.zio" %% "zio" % "1.0.8"
+libraryDependencies += "dev.zio" %% "zio-streams" % "1.0.8"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,44 @@
-import scala.annotation.switch
-import exercise2.Ior
+import zio.console._
 
-object Main extends App {
-  println("...")
+import exercise2._
+import scala.util.Success
+import scala.util.Failure
+import scala.annotation.tailrec
+import exercise2.lib.StdAudio
+import zio.{IO, UIO, URIO, ZIO, App}
+import zio.random._
+
+object MyApp extends App {
+
+  def run(args: List[String]) =
+    loop(whiteNoise()).run
+
+  def play(sample: Double): UIO[Unit] = ZIO.effectTotal(StdAudio.play(sample))
+
+  def whiteNoise(frequency: Int = 440, volume: Double = 1.0): URIO[Random, Queue[Double]] = {
+    if (frequency <= 0 && volume < 0.0 && volume > 1.0)
+      throw new Throwable("Incorrect parameters")
+
+    ZIO.foldLeft(0 to frequency)(new Queue[Double]()) { (acc, _) =>
+      for {
+        randomValue <- nextDoubleBetween(-0.5, 0.5)
+      } yield acc.enqueue(randomValue * volume)
+    }
+  }
+
+  def update(queue: Queue[Double]): Option[Queue[Double]] = {
+    val decay = 0.996
+    
+    queue.dequeue match {
+      case Success(newQueue) => Some(newQueue.enqueue((newQueue.front.get + queue.front.get) / 2 * decay))
+      case Failure (_) => None
+    }
+  }
+
+  @tailrec
+  def loop(queue: Queue[Double]): ZIO[Random, Throwable, Unit] = {
+    val updatedQueue = update(queue).get
+    play(updatedQueue.front.get)
+    loop(updatedQueue)
+  }
 }
