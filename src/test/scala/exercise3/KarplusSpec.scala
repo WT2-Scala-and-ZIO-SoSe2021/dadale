0daba50cf05f483b345a2ec9d914d280f7b80425
Darwin Becker <darwinbecker@web.de>
2021-06-10 17:09:10 +0200

    finished bonus task zio-test

--- src/test/scala/exercise3/KarplusSpec.scala
+++ src/test/scala/exercise3/KarplusSpec.scala
@@ -3,6 +3,7 @@ package exercise3
 import zio._
 import zio.test._
 import zio.test.Assertion._
+import zio.test.environment.TestRandom
 import exercise3.KarplusStrong._
 
 object KarplusSpec extends DefaultRunnableSpec {
@@ -21,22 +22,21 @@ object KarplusSpec extends DefaultRunnableSpec {
     },
 
     testM("Queue value should be between -0.5 and 0.5") {
-      val volume = .5
+      val randomDoubles = List(0.5, 0.2, 0.3)
+      val expectedRandoms = List(0.25, 0.1, 0.15)
       for {
-        queue <- whiteNoise(2, volume)
-      } yield assert(queue.front().get)(isLessThan(0.5 * volume) && isGreaterThan(-0.5 * volume))
-    },
-
-    // TODO: test throw exception
-    testM("Throws exception") {
-      val volume = 3.0
-      for {
-        queue <- whiteNoise(2, volume)
-        // https://stackoverflow.com/questions/58662285/how-to-test-an-exception-case-with-zio-test
-        result <- ZIO.fail("Incorrect parameters")
-      } yield assert(result)(fails(equalTo("Incorrect parameters")))
-      //assert(queue)(fails(equalTo("Incorrect parameters")))
+        _  <- TestRandom.feedDoubles(randomDoubles(0), randomDoubles(1), randomDoubles(2))
+        queue <- whiteNoise(3, 0.5)
+      } yield assert(expectedRandoms)(equalTo(List(queue.front().get, queue.dequeue().get.front().get, queue.dequeue().get.dequeue().get.front().get)))
     }
+
+    // To test exceptions in the whiteNoise function, it should return: ZIO[Random, IllegalArgumentException, Queue[Double]]
+    // Example:
+    //        testM("throws a runtime exception") {
+    //          for {
+    //            res <- ZIO.succeed(throw new IllegalArgumentException("unexpected error")).run
+    //          } yield assert(res)(dies(isSubtype[IllegalArgumentException](anything)))
+    //        }
 
   )
 
